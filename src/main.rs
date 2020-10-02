3eb9a433e05265b01345aea70820b17ab1c28ff3
Jesus Bracho <jessebracho@gmail.com>
2020-10-02 19:53:43 -0400

    Implement the rest of the load family

--- src/main.rs
+++ src/main.rs
@@ -56,12 +56,21 @@ fn main() {
 
     println!("{:b}", -9i16);
 
+    let window = pancurses::initscr();
+    window.printw("Hello Rust");
+    window.refresh();
+    window.getch();
+    pancurses::endwin();
+
     loop {
         let instruction: u16 = memory[registers[Register::ProgramCounter as usize] as usize];
         let operation = num::FromPrimitive::from_u16(instruction >> 12).unwrap();
 
         match operation {
             Op::ADD => op_add(instruction, &mut registers),
+            Op::LD => op_ld(instruction, &mut registers, &mut memory),
+            Op::LDI => op_ldi(instruction, &mut registers, &mut memory),
+            Op::LEA => op_lea(instruction, &mut registers),
             _ => (),
         }
     }
@@ -87,17 +96,73 @@ fn op_add(instruction: u16, registers: &mut RegisterMemory) {
     update_flags(dr, registers);
 }
 
-fn op_ldi(instruction: u16, registers: &mut RegisterMemory, memory: &mut Memory) {
-    let dr: u16 = (instruction >> 9) & 0b0000_0000_0000_0111;
-    let pc_offset: u16 = sign_extend(instruction & 0b0000_0001_1111_1111, 9);
-    registers[dr as usize] = mem_read(
-        mem_read(
-            registers[Register::ProgramCounter as usize] + pc_offset,
-            memory,
-        ),
-        memory,
-    );
+/// An address is computed by sign-extending bits [8:0] to 16 bits and adding
+/// this value to the incremented PC. The contents of memory at this address
+/// are loaded into DR. The condition codes are set, based on whether the value
+/// loaded is negative, zero, or positive.
+///
+/// ## Example
+///
+/// LD R4, VALUE ; R4 ← mem[VALUE]
+fn op_ld(instruction: u16, registers: &mut RegisterMemory, memory: &mut Memory) {
+    let dr = (instruction >> 9) & 0b111;
+    let pc_offset = instruction & 0b1_1111_1111;
+    let pc = registers[Register::ProgramCounter as usize];
+    let read_addr = pc + sign_extend(pc_offset, 9);
+
+    registers[dr as usize] = memory[read_addr as usize];
     update_flags(dr, registers);
+}
+
+/// An address is computed by sign-extending bits [8:0] to 16 bits and adding
+/// this value to the incremented PC. What is stored in memory at this address
+/// is the address of the data to be loaded into DR. The condition codes are
+/// set, based on whether the value loaded is negative, zero, or positive.
+///
+/// ## Example
+///
+/// LDI R4, ONEMORE ; R4 ← mem[mem[ONEMORE]]
+fn op_ldi(instruction: u16, registers: &mut RegisterMemory, memory: &mut Memory) {
+    let dr = (instruction >> 9) & 0b111;
+    let pc_offset = instruction & 0b1_1111_1111;
+    let pc = registers[Register::ProgramCounter as usize];
+    let read_addr = pc + sign_extend(pc_offset, 9);
+
+    registers[dr as usize] = mem_read(mem_read(read_addr, memory), memory);
+    update_flags(dr, registers);
+}
+
+/// An address is computed by sign-extending bits [5:0] to 16 bits and adding
+/// this value to the contents of the register specified by bits [8:6]. The
+/// contents of memory at this address are loaded into DR. The condition codes
+/// are set, based on whether the value loaded is negative, zero, or positive.
+///
+/// ## Example
+///
+/// LDR R4, R2, #−5 ; R4 ← mem[R2 − 5]
+fn op_ldr(instruction: u16, registers: &mut RegisterMemory, memory: &mut Memory) {
+    let dr = (instruction >> 9) & 0b111;
+    let base_r = (instruction >> 6) & 0b111;
+    let pc_offset = instruction & 0b11_1111;
+    let read_addr = base_r + sign_extend(pc_offset, 6);
+
+    registers[dr as usize] = mem_read(read_addr, memory);
+    update_flags(dr, registers);
+}
+
+/// An address is computed by sign-extending bits [8:0] to 16 bits and adding
+/// this value to the incremented PC. This address is loaded into DR. The
+/// condition codes are set, based on whether the value loaded is negative,
+/// zero, or positive.
+///
+/// LEA R4, TARGET ; R4 ← address of TARGET.
+fn op_lea(instruction: u16, regs: &mut RegisterMemory) {
+    let dr = (instruction >> 9) & 0b111;
+    let pc_offset = instruction & 0b1_1111_1111;
+    let pc = regs[Register::ProgramCounter as usize];
+
+    regs[dr as usize] = pc + sign_extend(pc_offset, 9);
+    update_flags(dr, regs);
 }
 
 /// Video explanation of the two's complement.
