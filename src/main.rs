fa8307c07903e65b488dc46c841beab48b05982e
Jesus Bracho <jessebracho@gmail.com>
2020-10-03 10:14:24 -0400

    Alphabetize opcodes

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,22 @@ enum MemoryMappedRegister {
 
 #[derive(num_derive::FromPrimitive)]
 enum Op {
-    BR,
-    ADD,
-    LD,
-    ST,
-    JSR,
-    AND,
-    LDR,
-    STR,
-    RTI,
-    NOT,
-    LDI,
-    STI,
-    JMP,
-    RES,
-    LEA,
-    TRAP,
+    ADD = 0b0001,
+    AND = 0b0101,
+    BR = 0b0000,
+    JMP = 0b1100,
+    JSR = 0b0100,
+    LD = 0b0010,
+    LDI = 0b1010,
+    LDR = 0b0110,
+    LEA = 0b1110,
+    NOT = 0b1001,
+    RES = 0b1101,
+    RTI = 0b1000,
+    ST = 0b0011,
+    STI = 0b1011,
+    STR = 0b0111,
+    TRAP = 0b1111,
 }
 
 enum Condition {
